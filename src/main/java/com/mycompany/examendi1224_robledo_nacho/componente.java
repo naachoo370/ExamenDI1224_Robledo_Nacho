81cc5cc52d5b969ada52449805d020d972b73b2e
nacho <nacho17insti@gmail.com>
2024-12-12 11:44:41 +0100

    examen

--- src/main/java/com/mycompany/examendi1224_robledo_nacho/componente.java
+++ src/main/java/com/mycompany/examendi1224_robledo_nacho/componente.java
@@ -5,32 +5,28 @@ import javax.swing.JLabel;
 
 public class componente extends JLabel {
 
-    // Constructor con texto por defecto
     public componente() {
         super("Texto predeterminado");
         configurarEstilo();
     }
 
-    // Constructor con texto personalizado
     public componente(String texto) {
         super(texto);
         configurarEstilo();
     }
 
-    // Configura los estilos predeterminados
     private void configurarEstilo() {
-        setBackground(new Color(85, 130, 243)); // Fondo azul
-        setOpaque(true); // Fondo visible
-        setHorizontalAlignment(CENTER); // Centrar el texto
-        setForeground(Color.BLACK); // Color de texto predeterminado
+        setBackground(new Color(85, 130, 243)); 
+        setOpaque(true); 
+        setHorizontalAlignment(CENTER); 
+        setForeground(Color.BLACK);
     }
 
-    // Cambia el color del texto según el valor ingresado
     public void cambiarColorSegunValor(int valor) {
         if (valor < 5) {
-            setForeground(Color.RED); // Rojo si el valor es menor que 5
+            setForeground(Color.RED); 
         } else {
-            setForeground(Color.BLACK); // Negro si el valor es mayor o igual a 5
+            setForeground(Color.BLACK); 
         }
     }
 }
